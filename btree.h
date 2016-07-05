7b9d83f4c38d8a6ab24132beb26a2106faacaa10
flouthoc <flouthoc.git@gmail.com>
2016-07-05 10:41:47 +0530

    Edit

--- btree.h
+++ btree.h
@@ -1,1 +1,16 @@
+#ifndef BTREE_H
+#define BTREE_H
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
+
+struct TreeNode{
+
+	size_t *children;
+	size_t *value;
+	char *key;
+};
